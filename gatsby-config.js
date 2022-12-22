380d3dbbc9d34266d3b39fe73e1ab95f5b040dc5
SujayPrabhu96 <prabhusujay@gmail.com>
2022-12-22 22:55:12 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `integrate-gatsby-google-tagmanager-using-partytown`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
