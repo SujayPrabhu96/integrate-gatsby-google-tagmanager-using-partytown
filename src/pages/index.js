a2aaffc7c3b19e207f7b7b228be32456bc1b060b
SujayPrabhu96 <prabhusujay@gmail.com>
2023-02-06 16:04:33 +0530

    Remove buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,9 @@
 import * as React from "react";
 
-const onClickHandler = () => {
-  window.dataLayer.push({
-    event: "select_item",
-    ecommerce: {
-      item_list_id: "rooms_listing",
-      item_list_name: "Rooms Listing",
-    },
-  });
-};
-
 const IndexPage = () => {
   return (
     <main>
       <h1>Index Page</h1>
-      <button onClick={onClickHandler}>Click here</button>
     </main>
   );
 };
